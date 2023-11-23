0497b86442a7f2119d109d6fefb1047b73612b2c
yeonji-0120 <noyeonji43@gmail.com>
2023-11-23 19:36:34 +0900

    2023.11.23

--- js/productAdd.js
+++ js/productAdd.js
@@ -74,3 +74,17 @@ $("#file").on("change", function(e){
     }
 });
 
+$('#summernote').summernote({
+    placeholder: 'Hello stand alone ui',
+    tabsize: 2,
+    height: 120,
+    toolbar: [
+      ['style', ['style']],
+      ['font', ['bold', 'underline', 'clear']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']],
+      ['table', ['table']],
+      ['insert', ['link', 'picture', 'video']],
+      ['view', ['fullscreen', 'codeview', 'help']]
+    ]
+  });
