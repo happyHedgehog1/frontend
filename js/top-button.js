edc7780786cede9d19c8b161ef7846dabd4bdd8a
JunKyeong-Lee <worldreaming@gmail.com>
2023-11-24 11:03:45 +0900

    2023년 11월 24일 이준경 개인 깃허브에 작성하던 내용 업데이트

--- js/top-button.js
+++
@@ -1,6 +0,0 @@
-const topButton = document.querySelector(".top-button");
-
-topButton.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
