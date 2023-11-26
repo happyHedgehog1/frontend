f6dc0ec3425287126ff5a5e04e15ed7e94ab535c
yeonji-0120 <noyeonji43@gmail.com>
2023-11-26 23:35:16 +0900

    2023.11.26

---
+++ js/answerModify.js
@@ -0,0 +1,14 @@
+$('#summernote').summernote({
+    placeholder: '',
+    tabsize: 2,
+    height: 300,
+    toolbar: [
+      ['style', ['style']],
+      ['font', ['bold', 'underline', 'clear']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']],
+      ['table', ['table']],
+      ['insert', ['link', 'picture', 'video']],
+      ['view', ['fullscreen', 'codeview', 'help']]
+    ]
+  });
